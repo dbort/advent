e6e743cf1c133629eb4a15fbb9fad51ab6fd9fc1
Dave Bort <git@dbort.com>
2021-12-22 21:18:47 -0800

    Day 8: 1

---
+++ day08/src/main.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+struct Display {
+    inputs: Vec<String>,
+    output: Vec<String>,
+}
+
+impl Display {
+    fn new(line: &str) -> Self {
+        let parts: Vec<String> = line.split(" | ").map(|s| s.to_string()).collect();
+        assert_eq!(parts.len(), 2);
+        Display {
+            inputs: parts[0].split(' ').map(|s| s.to_string()).collect(),
+            output: parts[1].split(' ').map(|s| s.to_string()).collect(),
+        }
+    }
+}
+
+fn first(input: &String) {
+    let mut count = 0;
+    for line in input.lines() {
+        let display = Display::new(line);
+        for d in display.output {
+            match d.len() {
+                7 | 4 | 3 | 2 => count += 1,
+                _ => (),
+            }
+        }
+    }
+    println!("Count: {}", count);
+}
+
+fn second(input: &String) {
+    for line in input.lines() {
+    }
+}
+
+fn main() {
+    let input = common::input::load_file("input.txt");
+    first(&input);
+    second(&input);
+}
