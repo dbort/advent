deade3f3a5ed73bdc32f24ddd3bb8b135eefbd7c
Dave Bort <git@dbort.com>
2021-12-22 20:36:09 -0800

    Day 6: Refactor; part 2 needs more work

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -27,28 +27,38 @@ fn format_population(fishes: &Vec<Fish>) -> String {
         .join(",")
 }
 
-fn first(input: &String) {
-    for line in input.lines() {
-        let start: Vec<u8> = line.split(',').map(|s| s.parse::<u8>().unwrap()).collect();
+/// Returns the number of fish after `days` days.
+fn simulate(start: &Vec<u8>, days: usize) -> usize {
+    let mut fish: Vec<Fish> = start.iter().map(|n| Fish::new(*n)).collect();
+    println!("Initial state: {}", format_population(&fish));
 
-        let mut fish: Vec<Fish> = start.iter().map(|n| Fish::new(*n)).collect();
-        println!("Initial state: {}", format_population(&fish));
-
-        for _day in 1..=80 {
-            let mut new_fish: Vec<Fish> = vec![];
-            for f in &mut fish {
-                if f.tick() {
-                    new_fish.push(Fish::new(8));
-                }
+    for _day in 1..=days {
+        let mut new_fish: Vec<Fish> = vec![];
+        for f in &mut fish {
+            if f.tick() {
+                new_fish.push(Fish::new(8));
             }
-            fish.append(&mut new_fish);
-            // println!("After {} days: [{}] {}", day, fish.len(), format_population(&fish));
         }
-        println!("Final: [{}]", fish.len());
+        fish.append(&mut new_fish);
+        // println!("After {} days: [{}] {}", day, fish.len(), format_population(&fish));
     }
+    println!("Final: [{}]", fish.len());
+    return fish.len();
 }
 
-fn second(_input: &String) {
+fn first(input: &String) {
+    let start: Vec<u8> = input.trim().split(',').map(|s| s.parse::<u8>().unwrap()).collect();
+    simulate(&start, 80);
+}
+
+fn second(input: &String) {
+    let start: Vec<u8> = input.trim().split(',').map(|s| s.parse::<u8>().unwrap()).collect();
+    let mut total = 0;
+    for (i, f) in start.iter().enumerate() {
+        println!("{}/{}...", i, start.len());
+        total += simulate(&vec![*f], 256);
+    }
+    println!("After 256 days: {}", total);
 }
 
 fn main() {
