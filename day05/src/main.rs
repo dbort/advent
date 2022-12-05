296ab125c33c80ce0d36111a6cb308a4869d4c5d
Dave Bort <git@dbort.com>
2022-12-04 22:00:22 -0800

    day05: part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -47,6 +47,29 @@ impl Stacks {
             self.stacks[dst].push(c);
         }
     }
+
+    fn execute2(&mut self, command: &str) {
+        let nums: Vec<usize> = command
+            .split(' ')
+            .filter_map(|s| match s.parse::<usize>() {
+                Ok(n) => Some(n),
+                Err(_) => None,
+            })
+            .collect();
+        if nums.len() != 3 {
+            panic!("Bad command {command}");
+        }
+        let count = nums[0];
+        let src = nums[1] - 1;
+        let dst = nums[2] - 1;
+        let mut v: Vec<char> = Vec::new();
+        for _ in 0..count {
+            v.push(self.stacks[src].pop().unwrap());
+        }
+        for c in v.iter().rev() {
+            self.stacks[dst].push(*c);
+        }
+    }
 }
 
 fn first(input: &String) {
@@ -74,7 +97,30 @@ fn first(input: &String) {
     println!("Tops: {tops}");
 }
 
-fn second(input: &String) {}
+fn second(input: &String) {
+    let mut stack_spec: Vec<&str> = Vec::new();
+    let mut getting_stacks: bool = true;
+    let mut stacks = Stacks { stacks: Vec::new() };
+    for line in input.lines() {
+        if getting_stacks {
+            if line.len() > 0 {
+                stack_spec.push(line);
+            } else {
+                stacks = Stacks::new(&stack_spec);
+                println!("Made stacks {:?}", stacks);
+                getting_stacks = false;
+            }
+        } else {
+            stacks.execute2(line);
+        }
+    }
+    println!("Final stacks {:?}", stacks);
+    let mut tops = String::new();
+    for stack in stacks.stacks {
+        tops.push(*stack.last().unwrap());
+    }
+    println!("Tops: {tops}");
+}
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
