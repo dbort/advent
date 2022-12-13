c06c77eb73e5a2a079f91268151d5ebac9ffcf02
Dave Bort <git@dbort.com>
2022-12-13 00:43:53 -0500

    day08: part 2

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -94,6 +94,58 @@ impl Forest {
         }
         Visibility { trees }
     }
+
+    fn scenic(&self) -> Vec<usize> {
+        let mut scores: Vec<usize> = Vec::new();
+        let rows = self.trees.len();
+        let cols = self.trees[0].len();
+        println!("rows {rows} x cols {cols}");
+        for row in 0..rows {
+            for col in 0..cols {
+                let height = self.trees[row][col];
+                let mut count_score = 1;
+                // Left
+                let mut count = 0;
+                for i in (0..col).rev() {
+                    count += 1;
+                    if self.trees[row][i] >= height {
+                        break;
+                    }
+                }
+                count_score *= count;
+                count = 0;
+                // Right
+                for i in (col + 1)..cols {
+                    count += 1;
+                    if self.trees[row][i] >= height {
+                        break;
+                    }
+                }
+                count_score *= count;
+                count = 0;
+                // Top
+                for i in (0..row).rev() {
+                    count += 1;
+                    if self.trees[i][col] >= height {
+                        break;
+                    }
+                }
+                count_score *= count;
+                count = 0;
+                // Bottom
+                for i in (row + 1)..rows {
+                    count += 1;
+                    if self.trees[i][col] >= height {
+                        break;
+                    }
+                }
+                count_score *= count;
+                scores.push(count_score);
+            }
+        }
+        scores.sort_unstable_by(|a, b| b.cmp(a));
+        scores
+    }
 }
 
 fn first(input: &String) {
@@ -104,7 +156,12 @@ fn first(input: &String) {
     println!("visible trees: {}", visibility.sum());
 }
 
-fn second(input: &String) {}
+fn second(input: &String) {
+    let forest = Forest::new(input);
+    println!("{:?}", forest);
+    let scores = forest.scenic();
+    println!("top score: {}", scores[0]);
+}
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
