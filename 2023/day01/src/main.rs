d117c9e9ebec6f821f47514673653270086c9583
Dave Bort <git@dbort.com>
2023-12-08 20:51:30 -0800

    day01: part 2 wip

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -1,27 +1,109 @@
+fn line_val(line: &str) -> u32 {
+    let mut first: u32 = 10;
+    let mut last: u32 = 10;
+    for c in line.chars() {
+        if c.is_digit(10) {
+            last = c.to_digit(10).unwrap();
+            if first > 9 {
+                first = last;
+            }
+        }
+    }
+    assert!(first <= 9, "No digits in '{}'", line);
+    return first * 10 + last;
+}
+
 fn first(input: &String) {
     let mut sum: u64 = 0;
     for line in input.lines() {
-        let mut first: u32 = 10;
-        let mut last: u32 = 10;
-        for c in line.chars() {
-            if c.is_digit(10) {
-                last = c.to_digit(10).unwrap();
-                if first > 9 {
-                    first = last;
-                }
-            }
-        }
-        assert!(first <= 9, "No digits in '{}'", line);
-        let value: u32 = first * 10 + last;
-        sum += value as u64;
+        sum += line_val(line) as u64;
     }
-    println!("Total: {}", sum);
+    println!("First total: {}", sum);
 }
 
-fn second(input: &String) {}
+struct Number {
+    word: String,
+    digit: i8,
+}
+
+fn second(input: &String) {
+    let numbers: Vec<_> = vec![
+        Number {
+            word: "one".to_string(),
+            digit: 1,
+        },
+        Number {
+            word: "two".to_string(),
+            digit: 2,
+        },
+        Number {
+            word: "three".to_string(),
+            digit: 3,
+        },
+        Number {
+            word: "four".to_string(),
+            digit: 4,
+        },
+        Number {
+            word: "five".to_string(),
+            digit: 5,
+        },
+        Number {
+            word: "six".to_string(),
+            digit: 6,
+        },
+        Number {
+            word: "seven".to_string(),
+            digit: 7,
+        },
+        Number {
+            word: "eight".to_string(),
+            digit: 8,
+        },
+        Number {
+            word: "nine".to_string(),
+            digit: 9,
+        },
+    ];
+    let mut sum: u64 = 0;
+    for linei in input.lines() {
+        let mut first: u32 = 10;
+        let mut last: u32 = 10;
+        let mut line: &str = linei;
+        while line.len() > 0 {
+            let mut d: u32 = 10;
+            let c = line.chars().nth(0).unwrap();
+            if c.is_digit(10) {
+                d = c.to_digit(10).unwrap();
+                line = &line[1..];
+            } else {
+                for n in &numbers {
+                    if line.starts_with(&n.word) {
+                        d = n.digit as u32;
+                        line = &line[n.word.len()..];
+                        break;
+                    }
+                }
+            }
+            if d < 10 {
+                if first > 9 {
+                    first = d;
+                }
+                last = d;
+            } else {
+                line = &line[1..];
+            }
+        }
+        assert!(first < 10 && last < 10, "Didn't find numbers for {}", line);
+        let val = first as u64 * 10 + last as u64;
+        println!("{} -> {}", linei, val);
+        sum += first as u64 * 10 + last as u64;
+    }
+    println!("Second total: {}", sum);
+}
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    first(&input);
+    // first(&input);
     second(&input);
 }
