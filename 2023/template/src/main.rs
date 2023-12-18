40b8fb09c75862be1edcde64c3d6fb7f243078eb
Dave Bort <git@dbort.com>
2023-12-18 13:47:24 -0500

    template refactor: print sums in main

--- 2023/template/src/main.rs
+++ 2023/template/src/main.rs
@@ -1,15 +1,17 @@
-fn first(input: &String) {
+fn part1(input: &String) -> i64 {
     let mut sum: i64 = 0;
     for line in input.lines() {
         sum += 1;
     }
-    println!("Total: {}", sum);
+    sum
 }
 
-fn second(input: &String) {}
+fn part2(input: &String) -> i64 {
+    -1
+}
 
 fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
-    first(&input);
-    second(&input);
+    let input = std::fs::read_to_string("sample-input.txt").unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
