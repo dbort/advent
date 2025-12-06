e8793b7f425f08938a284f2e0e54710a1e2c864e
Dave Bort <git@dbort.com>
2025-12-06 11:07:37 -0800

    control input and part from args

--- 2025/run.sh
+++ 2025/run.sh
@@ -4,9 +4,13 @@ set -o pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+DEFAULT_ARGS="sample-input.txt"
+NEW_ARGS=${@:-$DEFAULT_ARGS}
+set -- $NEW_ARGS
+
 cd "${SCRIPT_DIR}"
 readonly DAY="$(ls -d day?? | tail -1)"
 cd "${DAY}"
 cargo fmt
 cd "src"
-RUST_BACKTRACE=1 cargo run
+RUST_BACKTRACE=1 cargo run -- "$@"
--- 2025/template/src/main.rs
+++ 2025/template/src/main.rs
@@ -12,7 +12,26 @@ fn part2(input: &String) -> i64 {
 }
 
 fn main() {
-    let input = std::fs::read_to_string("sample-input.txt").unwrap();
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename> [<part>]", args[0]);
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+
+    let part = if args.len() >= 3 {
+        args[2].parse::<u32>().expect("Failed to parse part number")
+    } else {
+        3
+    };
+
+    println!("Using input from {}", filename);
+    let input = std::fs::read_to_string(filename).unwrap();
+
+    if part == 1 || part > 2 {
+        println!("Part 1: {}", part1(&input));
+    }
+    if part == 2 || part > 2 {
+        println!("Part 2: {}", part2(&input));
+    }
 }
