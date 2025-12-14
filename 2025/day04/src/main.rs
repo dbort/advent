bba0f2b22c1d9c4c6fb8d542e4a1133172037a66
Dave Bort <git@dbort.com>
2025-12-13 18:22:31 -0800

    day04: part 2

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -51,9 +51,56 @@ fn part1(input: &String) -> i64 {
     count
 }
 
+fn count_and_remove(grid: &mut Vec<Vec<bool>>) -> i64 {
+    let mut count = 0;
+    let rows = grid.len();
+    let cols = grid[0].len();
+    for y in 0..rows {
+        for x in 0..cols {
+            let mut neighbors = 0;
+            print!("(y {}, x {}) => ", y, x);
+            if !grid[y][x] {
+                println!("x");
+                continue;
+            }
+            for i in if y > 0 { y - 1 } else { y }..=if y < rows - 1 { y + 1 } else { y } {
+                for j in if x > 0 { x - 1 } else { x }..=if x < cols - 1 { x + 1 } else { x } {
+                    print!("[i {}, j {}]", i, j);
+                    if (i, j) != (y, x) {
+                        if grid[i][j] {
+                            neighbors += 1;
+                            print!("@, ");
+                        } else {
+                            print!("_, ");
+                        }
+                    } else {
+                        print!("., ");
+                    }
+                }
+            }
+            println!("");
+            if neighbors < 4 {
+                count += 1;
+                grid[y][x] = false;
+            }
+        }
+    }
+    count
+}
+
 fn part2(input: &String) -> i64 {
-    let _ = input;
-    -1
+    let nonmut_grid = parse_grid(input);
+
+    let mut grid: Vec<Vec<bool>> = nonmut_grid.iter().map(|i| i.clone()).collect();
+    let mut count = 0;
+    while true {
+        let new_count = count_and_remove(&mut grid);
+        if new_count == 0 {
+            break;
+        }
+        count += new_count;
+    }
+    count
 }
 
 fn main() {
